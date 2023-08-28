3e4547d3ecf76d190578072e1bd5d4ed8d3fb31b
Jonathas Santos <jonathaspe@hotmail.com>
2023-08-28 16:10:54 +0100

    Criação de um novo endpoint para a api

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/oi", function (req, res) {
+    res.send("Olá, Mundo!");
+  });
+
+app.listen(3000, () =>
+ console.log("Servidor rodando em http://localhost:3000/"));
